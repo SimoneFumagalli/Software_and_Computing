9bc68960fe41b9c605d02f25b1441577e338c10e
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-03-04 18:54:43 +0100

    Added import and MNIST dataset

--- BCM_classification.py
+++ BCM_classification.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  4 18:05:52 2022
+from plasticity.model import BCM
+from plasticity.model.optimizer import Adam
+from plasticity.model.weights import GlorotNormal, Normal
+import numpy as np
+import pylab as plt
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.datasets import fetch_openml
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-@author: Simone Fumagalli
-"""
+X, y = fetch_openml(name='mnist_784', version=1, data_id=None, return_X_y=True)
+    
+X*=1./255 # Normalization of the X inputs in range [0,1]
 
