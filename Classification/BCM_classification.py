059d22f70f01e5dd714c9d1e1eb83f7dc8b1d17c
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-03-10 17:11:26 +0100

    Error fixed + testing added

--- Classification/BCM_classification.py
+++ Classification/BCM_classification.py
@@ -1,6 +1,6 @@
 from plasticity.model import BCM
 from plasticity.model.optimizer import Adam
-from plasticity.model.weights import GlorotNormal, Normal
+from plasticity.model.weights import GlorotNormal
 import numpy as np
 import pylab as plt
 from sklearn.preprocessing import OneHotEncoder
@@ -8,10 +8,9 @@ from sklearn.datasets import fetch_openml
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-
 X, y = fetch_openml(name='mnist_784', version=1, data_id=None, return_X_y=True)
-    
-X*=1./255 # Normalization of the X inputs in range [0,1]
+
+X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
 
 #Transformation of the y vector
 y_int = y.astype('int')
@@ -19,54 +18,53 @@ y_reshape = y_int.values.reshape(-1,1)
 y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
 
 x_train, x_test, y_train, y_test = \
-    train_test_split(X, y_transform, test_size=1./8, random_state=42)
+train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
 
-#FITTING
+
 model = BCM(outputs=1000, num_epochs=10, optimizer=Adam(lr=4e-2), 
-            interaction_strength=0.,
-            weights_init=GlorotNormal(),
-            activation='Relu', batch_size=10000)
-
+                interaction_strength=0.,
+                weights_init=GlorotNormal(),
+                activation='Relu', batch_size=10000)
+    
 model.fit(x_train, y_train)
+   
 
 #PREDICTION
 
 def predict(X,y,x_predict):
-    while type(x_predict) == int and x_predict < len(y_test):
-        if True:
-            predict = model.predict(x_test.values[x_predict].reshape(1, -1), \
-                                    y=np.zeros_like(y_test[x_predict].reshape(1, -1)))
-        
-            # select the neuron connection with the highest response
-            highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
-            
-            # collecting the predicted labels
-            labels = model.weights[:, 28*28:].argmax(axis=1)
-            
-            #sorting the label with the highest response
-            predicted_label = sorted(zip(predict.ravel(), labels), \
-                                     key=lambda x : x[0], reverse=True)[:1]
-                
-            predicted_label = [x[1] for x in predicted_label]
-            
-            nc = np.amax(np.abs(model.weights))
-            
-            #plotting
     
-            fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 12))
-            ax1.set_title('Image from the MNIST dataset:{:d}'.format(y_test[x_predict].argmax()))
-            ax1.imshow(x_test.values[x_predict].reshape(28, 28), cmap='gray'); ax1.axis('off')
+    if type(x_predict) == int and x_predict < len(y_test):
+        predict = model.predict(x_test.values[x_predict].reshape(1, -1), \
+                                y=np.zeros_like(y_test[x_predict].reshape(1, -1)))
+    
+        # select the neuron connection with the highest response
+        highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
         
-            ax2.set_title('Prediction using BCM:{:d}'.format(predicted_label[0]))
-            ax2.imshow(highest_response, cmap='bwr', vmin=-nc, vmax=nc); ax2.axis('off')
+        # collecting the predicted labels
+        labels = model.weights[:, 28*28:].argmax(axis=1)
+        
+        #sorting the label with the highest response
+        predicted_label = sorted(zip(predict.ravel(), labels), \
+                                 key=lambda x : x[0], reverse=True)[:1]
             
-            break
+        predicted_label = [x[1] for x in predicted_label]
         
+        nc = np.amax(np.abs(model.weights))
+        
+        #plotting
+
+        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 12))
+        ax1.set_title('Image from the MNIST dataset:{:d}'.format(y_test[x_predict].argmax()))
+        ax1.imshow(x_test.values[x_predict].reshape(28, 28), cmap='gray'); ax1.axis('off')
+    
+        ax2.set_title('Prediction using BCM:{:d}'.format(predicted_label[0]))
+        ax2.imshow(highest_response, cmap='bwr', vmin=-nc, vmax=nc); ax2.axis('off')
+    
     else:
         print("Error: x_predict must be an integer number lower than", len(y_test)-1,\
-              ". Please, enter a valid number next time")
-    
+          ". Please, enter a valid number next time")
 predict(x_test, y_test, x_predict=0)
+
 
 ######## EVALUATION OF THE ACCURACY
 
--- Classification/testing_classification.py
+++ Classification/testing_classification.py
@@ -4,41 +4,79 @@ Created on Wed Mar  9 12:42:26 2022
 
 @author: Simone Fumagalli
 """
+from plasticity.model import BCM
+from plasticity.model.optimizer import Adam
+from plasticity.model.weights import GlorotNormal, Normal
 import numpy as np
 import pylab as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
-
+import pandas as pd
 
 X, y = fetch_openml(name='mnist_784', version=1, data_id=None, return_X_y=True)
+X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
 
-def variables(i):
+#Transformation of the y vector
+y_int = y.astype('int')
+y_reshape = y_int.values.reshape(-1,1)
+y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
+
+x_train, x_test, y_train, y_test = \
+train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
+
+def test_variables():
     
-    X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
+    assert isinstance(X, pd.DataFrame), "X must be a DataFrame"
+    assert isinstance(y, pd.Series), "y must be a Series type"
+    assert X.shape[0] == y.shape[0], "they must have the same number of rows"
+    assert len(x_train) == len(X)*(7/8)
+    assert len(y_train) == len(y)*(7/8)
+    assert len(x_test) == len(X)*(1/8)
+    assert len(y_test) == len(y)*(1/8)
+        
+
+model = BCM(outputs=1000, num_epochs=10, optimizer=Adam(lr=4e-2), 
+                        interaction_strength=0.,
+                        weights_init=GlorotNormal(),
+                        activation='Relu', batch_size=10000)
     
-    #Transformation of the y vector
-    y_int = y.astype('int')
-    y_reshape = y_int.values.reshape(-1,1)
-    y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
+model.fit(x_train, y_train)
+
+
+def test_predict():
+    x_predict=0
+    if type(x_predict) == int and x_predict < len(y_test):
+        
+        predict = model.predict(x_test.values[x_predict].reshape(1, -1), \
+                                y=np.zeros_like(y_test[x_predict].reshape(1, -1)))
     
-    x_train, x_test, y_train, y_test = \
-    train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
-    r = [x_train, y_train, x_test, y_test]
-    return len(r[i])
-    
+        # select the neuron connection with the highest response
+        highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
+        
+        # collecting the predicted labels
+        labels = model.weights[:, 28*28:].argmax(axis=1)
+        
+        #sorting the label with the highest response
+        predicted_label = sorted(zip(predict.ravel(), labels), \
+                                 key=lambda x : x[0], reverse=True)[:1]
+            
+        predicted_label = [x[1] for x in predicted_label]
+        nc = np.amax(np.abs(model.weights))
+        
+        assert len(predict) == 1
+        assert len(highest_response) == 28
+        assert type(x_predict) == int
+        
+    else:
+        assert type(x_predict) == float or str
+        print("Error: x_predict must be an integer number lower than", len(y_test)-1,\
+          ". Please, enter a valid number next time")
 
-def test_x_train():
-    assert variables(0) == len(X)*(7/8)
 
-def test_y_train():
-    assert variables(1) == len(y)*(7/8)
 
-def test_x_test():
-    assert variables(2) == len(X)*(1/8)
+        
 
-def test_y_test():
-    assert variables(3) == len(X)*(1/8)
 
 
 
