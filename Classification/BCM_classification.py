5ed152b0d12738ee2fc6f392a864a710f111081a
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-03-11 13:26:22 +0100

    Function construction

--- Classification/BCM_classification.py
+++ Classification/BCM_classification.py
@@ -2,43 +2,94 @@ from plasticity.model import BCM
 from plasticity.model.optimizer import Adam
 from plasticity.model.weights import GlorotNormal
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import fetch_openml
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import pylab as plt
-
-
 from sklearn.metrics import accuracy_score
 
+def variables(X,y, i):
+    """
+    This method splits the variables into a train set and test set.
+    
+    Parameters
+    ----------
+    X : Dataframe
+        Dataframe containing informations about the pixels of MNIST images.
+    
+    y : Series
+        List containing the labels related to the MNIST images.
+    
+    i : int
+        It indicates which variable the function returns.
 
-X, y = fetch_openml(name='mnist_784', version=1, data_id=None, return_X_y=True)
+    Returns
+    -------
+    The four variables x_train, y_train, x_test, y_test composing the train and
+    test sets.
+    """
+    
+    X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
+    
+    #Transformation of the y vector
+    y_int = y.astype('int')
+    y_reshape = y_int.values.reshape(-1,1)
+    y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
+    
+    #Splitting of the variables in training and test sets
+    x_train, x_test, y_train, y_test = \
+    train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
+    variable = [x_train, y_train, x_test, y_test]
+    return variable[i]
 
-X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
+def fitting(X,y):
+    """
+    This method define the model used for classification and uses the train
+    variables to train the model.
 
-#Transformation of the y vector
-y_int = y.astype('int')
-y_reshape = y_int.values.reshape(-1,1)
-y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
-
-x_train, x_test, y_train, y_test = \
-train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
-
-
-model = BCM(outputs=1000, num_epochs=10, optimizer=Adam(lr=4e-2), 
+    Parameters
+    ----------
+    X : Dataframe
+        Dataframe of the train set.
+    
+    y : Series
+        List containing the labels in train set.
+    
+    Returns
+    -------
+    The trained model.
+    """
+    model = BCM(outputs=1000, num_epochs=10, optimizer=Adam(lr=4e-2), 
                 interaction_strength=0.,
                 weights_init=GlorotNormal(),
                 activation='Relu', batch_size=10000)
     
-model.fit(x_train, y_train)
-   
+    model.fit(X,y)
+    return model
 
 #PREDICTION
 
-def predict(X,y,x_predict):
+def predict(X, y, model, x_predict):
+    """
+    This method uses the model to make predictions on a new set, the test set.
+
+    Parameters
+    ----------
+    X : Dataframe
+        Dataframe cointaining the test set.
     
-    if type(x_predict) == int and x_predict < len(y_test):
-        predict = model.predict(x_test.values[x_predict].reshape(1, -1), \
-                                y=np.zeros_like(y_test[x_predict].reshape(1, -1)))
+    y : Series
+        List containing the labels in test set.
+    
+    model : trained model used to make predictions.
+    
+    x_predict : int
+                Number corresponding to the MNIST image that must be shown.
+
+    """
+    
+    if type(x_predict) == int and x_predict < len(y):
+        predict = model.predict(X.values[x_predict].reshape(1, -1), \
+                                y=np.zeros_like(y[x_predict].reshape(1, -1)))
     
         # select the neuron connection with the highest response
         highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
@@ -57,29 +108,38 @@ def predict(X,y,x_predict):
         #plotting
 
         fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 12))
-        ax1.set_title('Image from the MNIST dataset:{:d}'.format(y_test[x_predict].argmax()))
-        ax1.imshow(x_test.values[x_predict].reshape(28, 28), cmap='gray'); ax1.axis('off')
+        ax1.set_title('Image from the MNIST dataset:{:d}'.format(y[x_predict].argmax()))
+        ax1.imshow(X.values[x_predict].reshape(28, 28), cmap='gray'); ax1.axis('off')
     
         ax2.set_title('Prediction using BCM:{:d}'.format(predicted_label[0]))
         ax2.imshow(highest_response, cmap='bwr', vmin=-nc, vmax=nc); ax2.axis('off')
     
     else:
-        print("Error: x_predict must be an integer number lower than", len(y_test)-1,\
+        print("Error: x_predict must be an integer number lower than", len(y)-1,\
           ". Please, enter a valid number next time")
-predict(x_test, y_test, x_predict=0)
+
 
 
 ######## EVALUATION OF THE ACCURACY
 
-def testing(x,y):
+def accuracy(X,y,model):
+    """
+    This method evaluates the accuracy of the prediction.
 
-    testing = model.predict(x_test, y_test)
+    Parameters
+    ----------
+    X : Dataframe
+        Dataframe containing the test set.
+    y : Series
+        List containing the labels in test set.
+    
+    model : trained model used to make predictions.
+    """
+    accuracy = model.predict(X, y)
 
-    y_values = [model.weights[np.argmax(x)][28*28:].argmax() for x in testing]
+    y_values = [model.weights[np.argmax(x)][28*28:].argmax() for x in accuracy]
 
-    y_true = y_test.argmax(axis=1)
+    y_true = y.argmax(axis=1)
     y_pred = np.asarray(y_values)
 
     print('Prediction Accuracy on the test set: {:.3f}'.format(accuracy_score(y_true, y_pred)))
-
-testing(x_test, y_test)
--- Classification/testing_classification.py
+++ Classification/testing_classification.py
@@ -4,85 +4,44 @@ Created on Wed Mar  9 12:42:26 2022
 
 @author: Simone Fumagalli
 """
-from plasticity.model import BCM
-from plasticity.model.optimizer import Adam
-from plasticity.model.weights import GlorotNormal
-import numpy as np
-import pylab as plt
-from sklearn.preprocessing import OneHotEncoder
+from BCM_classification import variables, fitting, predict, accuracy
 from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import pandas as pd
 
 X, y = fetch_openml(name='mnist_784', version=1, data_id=None, return_X_y=True)
-X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
 
-#Transformation of the y vector
-y_int = y.astype('int')
-y_reshape = y_int.values.reshape(-1,1)
-y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
-
-x_train, x_test, y_train, y_test = \
-train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
+x_train = variables(X, y, 0)
+y_train = variables(X, y, 1)
+x_test = variables(X, y, 2)
+y_test = variables(X, y, 3)
 
 def test_variables():
     
-    assert isinstance(X, pd.DataFrame), "X must be a DataFrame"
-    assert isinstance(y, pd.Series), "y must be a Series type"
-    assert X.shape[0] == y.shape[0], "they must have the same number of rows"
     assert len(x_train) == len(X)*(7/8)
     assert len(y_train) == len(y)*(7/8)
     assert len(x_test) == len(X)*(1/8)
     assert len(y_test) == len(y)*(1/8)
         
-
-model = BCM(outputs=1000, num_epochs=10, optimizer=Adam(lr=4e-2), 
-                        interaction_strength=0.,
-                        weights_init=GlorotNormal(),
-                        activation='Relu', batch_size=10000)
-    
-model.fit(x_train, y_train)
+model = fitting(x_train,y_train)
 
 def test_model():
+    
     assert isinstance(model.outputs, int)
     assert isinstance(model.num_epochs, int)
     assert isinstance(model.batch_size, int)
 
 def test_predict():
-    x_predict=0
-    if type(x_predict) == int and x_predict < len(y_test):
         
-        predict = model.predict(x_test.values[x_predict].reshape(1, -1), \
-                                y=np.zeros_like(y_test[x_predict].reshape(1, -1)))
-    
-        # select the neuron connection with the highest response
-        highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
-        
-        # collecting the predicted labels
-        labels = model.weights[:, 28*28:].argmax(axis=1)
-        
-        #sorting the label with the highest response
-        
-        nc = np.amax(np.abs(model.weights))
-        
-        
+        predict(x_test,y_test,model, x_predict)
         assert type(x_predict) == int
-        assert type(nc) == np.float64
         assert len(labels) == model.outputs
         
         
-    else:
+    
         assert type(x_predict) == float or str
 
 
 def test_accuracy():
-    testing = model.predict(x_test, y_test)
-
-    y_values = [model.weights[np.argmax(x)][28*28:].argmax() for x in testing]
-
-    y_true = y_test.argmax(axis=1)
-    y_pred = np.asarray(y_values)
     
     assert isinstance(y_values, list)
     assert isinstance(y_true, np.ndarray)
