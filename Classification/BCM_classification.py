bed2ec108e378a23b093aa0ec452b3c70df233c0
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-06-22 17:25:07 +0200

    First functions added

--- Classification/BCM_classification.py
+++ Classification/BCM_classification.py
@@ -1,165 +1,187 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  9 12:42:26 2022
+Created on Wed Jun  8 16:24:17 2022
 
 @author: Simone Fumagalli
 """
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 13:09:41 2022
+
+@author: Simone Fumagalli
+"""
+
+import numpy as np
 from plasticity.model import BCM
-from plasticity.model.optimizer import Adam
-from plasticity.model.weights import HeNormal
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-import numpy as np
+from sklearn.metrics import classification_report
+from plasticity.utils import view_weights
 import pylab as plt
-from sklearn.metrics import accuracy_score
+from collections import Counter
 
-def variables(X,y, i):
-    """
-    This method splits the variables into a train set and test set.
+class Classification():
+    '''
+    The idea of this class is to use the BCM model, created by Nico Curty et al.,
+    to classify correctly a given image.
+    Four major functions has been constructed in order to:
+        - Visualize the reached configuration of the neurons;
+        - Construct models using the train and test ( or validation) sets;
+        - Obtain the performances of the models;
+        - Visualize the input and output images, the last corresponding to the 
+          best neuron.
     
     Parameters
-    ----------
-    X : Dataframe
-        Dataframe containing informations about the pixels of MNIST images.
     
-    y : Series
-        List containing the labels related to the MNIST images.
+    All the parameters must take the array's form, except the number of 
+    attempts.
     
-    i : int
-        It indicates which variable the function returns.
-
-    Returns
-    -------
-    The four variables x_train, y_train, x_test, y_test composing the train and
-    test sets.
-    """
+    output: array of int
+    The number of neurons to be used.
     
-    X_1=X*(1./255) # Normalization of the X inputs in range [0,1]
+    batch: array of int
+    The dimension of the batch.
     
-    #Transformation of the y vector
-    y_int = y.astype('int')
-    y_reshape = y_int.values.reshape(-1,1)
-    y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
+    weights: array of BaseWeights object
+    The Weights' initialization strategy.
     
-    #Splitting of the variables in training and test sets
-    x_train, x_test, y_train, y_test = \
-    train_test_split(X_1, y_transform, test_size=1./8, random_state=42)
-    variable = [x_train, y_train, x_test, y_test]
-    return variable[i]
-
-def fitting(X,y):
-    """
-    This method define the model used for classification and uses the train
-    variables to train the model.
-
-    Parameters
-    ----------
-    X : Dataframe
-        Dataframe of the train set.
+    number_attempts: int
+    The number of times the model must be executed.
     
-    y : Series
-        List containing the labels in train set.
+    optimizer: array of Optimizer objects
+    Optimizer Objects.
     
-    Returns
-    -------
-    The trained model.
-    """
-    model = BCM(outputs=100, num_epochs=10, optimizer=Adam(lr=1e-3), 
-                interaction_strength=0.,
-                weights_init=HeNormal(),
-                activation='Relu', batch_size=100)
+    Interaction_strenght: array of int
+    Set the interaction strenght between the neurons.
     
-    model.fit(X,y) #training of the model
-    return model
-
-
-
-def predict(X, y, model, x_predict):
-    """
-    This method uses the model to make predictions on a new set, the test set.
-    It outputs the raw images and the predicted one.
+    number_epoch: array of int
+    The total number of epochs.
     
-    Parameters
-    ----------
-    X : Dataframe
-        Dataframe cointaining the test set.
-    
-    y : Series
-        List containing the labels in test set.
-    
-    model : trained model used to make predictions.
-    
-    x_predict : int
-                Number corresponding to the MNIST image that must be shown.
-    
-    Returns
-    -------
-    It returns the predicted labels. It is useful for testing purpose.
-    
-    """
-    
-    if type(x_predict) == int and x_predict < len(y):
-        predict = model.predict(X.values[x_predict].reshape(1, -1), \
-                                y=np.zeros_like(y[x_predict].reshape(1, -1)))
-    
-        # select the neuron connection with the highest response
-        highest_response = model.weights[np.argmax(predict)][:28*28].reshape(28, 28)
+    --------------------------------------------------------------------------
+    In order to go into the detail of the BCM model and of every parameter
+    created, look at the Repository 'Plasticity' of Nico-Curti on GitHub.
+    '''
+    def __init__(self, output : np.array, batch : np.array, weights : np.array, 
+                 number_attempts : int, optimizer : np.array, 
+                 interaction_strength : np.array, number_epoch : np.array):
         
-        # collecting the predicted labels
-        labels = model.weights[:, 28*28:].argmax(axis=1)
-        
-        #sorting the label with the highest response
-        predicted_label = sorted(zip(predict.ravel(), labels), \
-                                 key=lambda x : x[0], reverse=True)[:1]
+        self.output = output
+        self.batch = batch
+        self.weights = weights
+        self.number_attempts = number_attempts 
+        self.optimizer = optimizer
+        self.interaction_strength = interaction_strength
+        self.number_epoch = number_epoch
+
+    
+    def Variable_Reshape(self,X,y):
+        '''
+        Function to reshape the images and the labels that enter in the model.        
+
+        Parameters
+        ----------
+        X : Dataframe
+            Dataframe containing informations about the pixels of images.
             
-        predicted_label = [x[1] for x in predicted_label]
+        y : Series
+            List containing the labels related to the image.
+
+        Returns
+        -------
+        X_norm : Dataframe
+            Normalized version of the X set.
+        y_transform : List
+            Categorical form of the labels.
+
+        '''
+        X_norm=X*(1./255) # Normalization of the X inputs in range [0,1]
         
-        nc = np.amax(np.abs(model.weights))
+        # Transformation of the y vector
+        y_int = y.astype('int')
+        y_reshape = y_int.values.reshape(-1,1)
+        y_transform = OneHotEncoder(sparse=False).fit_transform(y_reshape)
+        return X_norm, y_transform
+
+    def Variable_Split(self, X, y, fraction: float):
+        '''
+        Function to split the input variables X and y in train and test set.        
+
+        Parameters
+        ----------
+        X : Dataframe
+            Dataframe containing informations about the pixels of images.
+            
+        y : List
+            List containing the labels related to the image.
+            
+        fraction : float
+            Fraction representing the ratio between the train and test dataset.
+
+        Returns
+        -------
+        The four variables x_train, y_train, x_test, y_test composing the train and
+        test sets.
+
+        '''
+        # Splitting of the variables in train and test sets
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=fraction,\
+                                                                random_state=42)  
         
-        #plotting
+        return x_train,\
+               x_test,\
+               y_train,\
+               y_test
+               
+    def modellization(self, num_model):
+        '''
+        Creation of the model using the parameters of the Classification class.
+        
+        Parameters
+        ----------
+        num_model : int
+            Parameter corresponding to position of the element of each model 
+            considered.
+        Returns
+        -------
+        Construction of the model using the parameters of the class.
 
-        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 12))
-        ax1.set_title('Image from the MNIST dataset:{:d}'.format(y[x_predict].argmax()))
-        ax1.imshow(X.values[x_predict].reshape(28, 28), cmap='gray'); ax1.axis('off')
-    
-        ax2.set_title('Prediction using BCM:{:d}'.format(predicted_label[0]))
-        ax2.imshow(highest_response, cmap='bwr', vmin=-nc, vmax=nc); ax2.axis('off')
-        return len(labels)
-    else:
-        print("Error: x_predict must be an integer number lower than", len(y)-1,\
-          ". Please, enter a valid number next time")
+        '''
+        
+        #Construction of the BCM model
+        model = [BCM(outputs= self.output[num_model], 
+                    num_epochs=self.number_epoch[num_model], 
+                    optimizer=self.optimizer[num_model], 
+                    interaction_strength=self.interaction_strength[num_model],
+                    weights_init=self.weights[num_model],
+                    activation='Relu', batch_size=self.batch[num_model],
+                    random_state=42)]
+        return model
+     
+    def fitting(self, X, model, y = None):
+        '''
+        Training of the model.
 
+        Parameters
+        ----------
+        X : Dataframe
+            Train Set of images.
+            
+        model : function object
+            BCM model.
+            
+        y : List
+            Train Set of Labels. The default is None.
 
+        Returns
+        -------
+        Trained model.
 
-
-def accuracy(X,y,model):
-    """
-    This method evaluates the accuracy of the prediction.
-
-    Parameters
-    ----------
-    X : Dataframe
-        Dataframe containing the test set.
-    y : Series
-        List containing the labels in test set.
-    
-    model : trained model used to make predictions.
-    
-    Returns
-    -------
-    It returns the  lenghts of the variable for testing purpose.
-    
-    """
-    accuracy = model.predict(X, y)
-    
-    # prediction of the labels
-    y_values = [model.weights[np.argmax(x)][28*28:].argmax() for x in accuracy]
-
-    y_true = y.argmax(axis=1)
-    
-    y_pred = np.asarray(y_values)
-    
-    length = [len(y_values), len(y_true), len(y_pred)]
-    print('Prediction Accuracy on the test set: {:.3f}'.format(accuracy_score(y_true, y_pred)))
-    return length
-    
+        '''
+        
+        if y is not None:
+            model_fit = model[0].fit(X,y) #Fitting the model in a supervised way
+        else:
+            model_fit = model[0].fit(X) # Fitting the model, 
+                                          # without the knowledge of the labels
+        return [model_fit]
