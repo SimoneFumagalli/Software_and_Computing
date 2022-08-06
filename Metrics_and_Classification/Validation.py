f25c94b45c7268f26f090e4f053b95191daecd58
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-08-06 15:36:06 +0200

    Added del function

--- Metrics_and_Classification/Validation.py
+++ Metrics_and_Classification/Validation.py
@@ -8,6 +8,8 @@ Created on Sat Aug  6 15:23:51 2022
 from sklearn.model_selection import StratifiedKFold
 import Metrics_and_Classification
 
+def __del__(self):
+    print(" ")
 def validation_sets(x_train,y_train, n_splits:int = 2):
     
     skf = StratifiedKFold(n_splits)
