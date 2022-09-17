a46333dec46c8fd072c540bfb4b085ae783358f6
Simone Fumagalli <simone.fumagalli@studio.unibo.it>
2022-09-17 10:46:49 +0200

    Added utils folder and testing_utils file

---
+++ BCM_Classification/Utils/Testing_utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 10:42:16 2022
+
+@author: Simone Fumagalli
+"""
+
+import numpy as np
+import Classification
+
+def ylabels(classifier):
+    y_lab = [classifier[0][np.argmax(x)][28*28:].argmax() 
+              for x in classifier[1]]
+    return y_lab
+
+def top_10_labels(classifier):
+    top_10 = Classification.top_ten(classifier)
+    ten_result = [top_10[x][0][1] for x in range(len(top_10))]
+    return ten_result
